b9f1217f9df93d68f4c617ea28ad24de06d5f14f
gfed53 <gfederico7153@gmail.com>
2016-05-15 16:42:19 -0400

    Added new error alert message

--- components/libraries.js
+++ components/libraries.js
@@ -5,38 +5,31 @@ angular
 
 function ahSearch($http, $q){
 	return function(searchTerm){
-		    var url = "http://www.tastekid.com/api/similar?callback=JSON_CALLBACK";
-		    var request = {
-				q: searchTerm,
-				k: "179625-Educatio-EE7ZUYWY",
-				info: 1
-		    };
-		    var services = {
-		    	getResults: getResults,
-		    	getResults2: getResults2
-		    };
-		    return services;
+	    var url = "http://www.tastekid.com/api/similar?callback=JSON_CALLBACK";
+	    var request = {
+			q: searchTerm,
+			k: "179625-Educatio-EE7ZUYWY",
+			info: 1
+	    };
+	    var services = {
+	    	getResults: getResults
+	    };
+	    return services;
 
-		    function getResults(){
-		    	return $http({
-		    		method: 'JSONP',
-		    		url: url,
-		    		params: request
-		    	})
-		    	.then(function(response){
-		    		var results = response;
-		    		console.log(response);
-		    		return $q.when(response);
-		    	},
-		    	function(response){
-		    		alert('error');
-		    	});
-		    }
-
-		    function getResults2(){
-		    	$.getJSON(url, request, function(data){
-		    		console.log(data);
-		    	});
-		    }
+	    function getResults(){
+	    	return $http({
+	    		method: 'JSONP',
+	    		url: url,
+	    		params: request
+	    	})
+	    	.then(function(response){
+	    		var results = response;
+	    		console.log(response);
+	    		return $q.when(response);
+	    	},
+	    	function(response){
+	    		alert("Sorry, an error occurred. Please try again later");
+	    	});
+	    }
 	}
 }
